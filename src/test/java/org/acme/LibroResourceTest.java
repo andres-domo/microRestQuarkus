ba8d6115134db0cad598e80ec16a0286c986ab49
ANDRES DOMINGUEZ MORIENTES <6001585@viewnext.com>
2021-01-15 14:44:20 +0100

    inicio test

--- src/test/java/org/acme/LibroResourceTest.java
+++ src/test/java/org/acme/LibroResourceTest.java
@@ -1,23 +1,26 @@
 package org.acme;
 
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.google.inject.Inject;
 
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-import java.net.URL;
+import java.util.List;
+import java.util.UUID;
 
+import org.acme.cliente.Libro;
 import org.acme.interfaces.LibroService;
 import org.acme.resources.LibroResource;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import io.quarkus.test.Mock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.common.http.TestHTTPResource;
+
 
 //import org.acme.interfaces.LibroService;
 
@@ -29,15 +32,21 @@ import io.restassured.http.ContentType;
 @QuarkusTest
 class LibroResourceTest {
 
+	@InjectMock
+	@RestClient
+	LibroService servicio;
 	
-	@TestHTTPEndpoint(LibroResource.class)
-	@TestHTTPResource
-	URL url;
+	@Inject
+	LibroResource libroresource;
+	@TestHTTPEndpoint(LibroResource.class)//hace que el path sea /book.
+	//@TestHTTPResource
+	//URL url;
+	
 	
 	 @Test
-	 public void testListado() {
+	 public void  testlistado() {
 		 given().contentType(ContentType.JSON)
-         	.when().get("/book/listado")
+         	.when().get("/listado")
          	.then()
          		.statusCode(200)    
          		.body(is("[]"));
@@ -45,14 +54,42 @@ class LibroResourceTest {
 	 }
 
 	 @Test
-	 public void testListado_unitario() {
-		 given().contentType(ContentType.JSON)
-         	.when().get("/book/listado/{id}")
-         	.then()
-         		.statusCode(200)    
-         		.body(is("null"));
+	 public void testlistado_unitario() {
 		 
+		Libro libro= new Libro(1,"los pajaros del avismo",45543242,null) ;
+		 
+		servicio.guardarlibro(libro);
+		
+		 
+		 given().contentType(ContentType.JSON)
+      		.when().get("/listado/1")
+      		.then()
+      			.statusCode(200)    
+      			.body(is(libro));
+         		
 	 }
 	 
+	/* @Test
+	 public void testGuardar() {
+		 
+		 Libro libro2= new Libro(2,"los pajaros del averno",45543272,null) ;
+		 
+		servicio.guardarlibro(libro2);
+		 
+		  given().contentType(ContentType.JSON)
+         	.when().get("/guardar")
+         	.then()
+         		.statusCode(200)    
+         		.body(is(libro2));
+		 
+	 }*/
+	/* @Test
+	    public void testActualizar() {
+		 given().contentType(ContentType.JSON)
+      		.when().get("/actualizar")
+      		.then()
+      			.statusCode(200)    
+      			.body(is("[]"));
+	    }	*/
 	 
 }
